b3d46fc05b7a4c469f8697bbbb1e33ed3034ca94
Gabriel Mazzoleni Pinto <mpgabriel95@gmail.com>
2020-03-11 11:14:54 -0300

    migrate flask

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Apply database migrations
-echo "Apply database migrations"
-python manage.py makemigrations
-python manage.py migrate
-
---
+++ services/web/project/blueprint/tokens.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, current_app, request, jsonify
+from ..model import Token
+from ..repository import token_repository as repository
+from ..serializer import TokenSchema
+from ..service import reset_token_service
+
+
+bp_tokens = Blueprint('tokens', __name__)
+
+@bp_tokens.route('/token/list', methods=['GET'])
+def list_all():
+    schema = TokenSchema(many=True)
+    result = repository.list_all()
+    return schema.jsonify(result), 200
+
+@bp_tokens.route('/token/<id>', methods=['GET'])
+def get(id):
+    schema = TokenSchema()
+    result = repository.get_by_id(id)
+    return schema.jsonify(result), 200
+
+@bp_tokens.route('/token/available', methods=['GET'])
+def get_available():
+    schema = TokenSchema()
+    result = reset_token_service.get_available_token()
+    return schema.jsonify(result), 200
+
+@bp_tokens.route('/token/', methods=['POST'])
+def save():
+    schema = TokenSchema()
+    token = schema.load(request.json)
+
+    result = repository.save(token)
+
+    return schema.jsonify(result), 201
+
+@bp_tokens.route('/token/<id>', methods=['PUT'])
+def update(id):
+    schema = TokenSchema()
+    result  = repository.update(id, request.json)
+
+    return schema.jsonify(result), 200
+
+@bp_tokens.route('/token/<id>', methods=['DELETE'])
+def delete(id):
+    repository.delete(id)
+
+    return '', 200
