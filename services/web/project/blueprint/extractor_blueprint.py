c68fa11cab1d50cb91ee2ac5195493ec45b0c4fd
Gabriel Mazzoleni Pinto <mpgabriel95@gmail.com>
2020-05-31 12:35:58 -0300

    add logs

--- services/web/project/blueprint/extractor_blueprint.py
+++ services/web/project/blueprint/extractor_blueprint.py
@@ -1,9 +1,13 @@
 from flask import Blueprint
 from ..service import extractor_service as service
+import time
+
 
 bp_extractor = Blueprint('extractor', __name__)
 
 @bp_extractor.route('/extract/<user>/<steps>', methods=['POST'])
 def extract(user, steps):
+    start_time = time.time()
     service.extract(user, steps)
+    print("--- extractor total time time:  %s seconds ---" % (time.time() - start_time))
     return '', 200
--- services/web/project/blueprint/metrics_blueprint.py
+++ services/web/project/blueprint/metrics_blueprint.py
@@ -1,24 +1,33 @@
 from flask import Blueprint
 from ..service import metrics_service, graph_service, semantic_metrics_service
+import time
 
 bp_metrics = Blueprint('metrics', __name__)
 
 @bp_metrics.route('/metrics/shared-repos', methods=['POST'])
 def calculate_shared_repos():
+    start_time = time.time()
     metrics_service.calculate_shared_repos()
+    print("--- shared-repos time:  %s seconds ---" % (time.time() - start_time))
     return '', 200
 
-@bp_metrics.route('/graph/init/<login>', methods=['POST'])
+@bp_metrics.route('/metrics/topological/<login>', methods=['POST'])
 def init_graph(login):
+    start_time = time.time()
     graph_service.load_graph(login)
+    print("--- topological metrics time:  %s seconds ---" % (time.time() - start_time))
     return '', 200
 
 @bp_metrics.route('/metrics/shared_contributions/<login>', methods=['POST'])
 def calculate_shared_contributions(login):
+    start_time = time.time()
     semantic_metrics_service.save_shared_contributions(login)
+    print("--- shared_contributions time:  %s seconds ---" % (time.time() - start_time))
     return '', 200
 
 @bp_metrics.route('/metrics/shared_pulls/<login>', methods=['POST'])
 def calculate_shared_pulls(login):
+    start_time = time.time()
     semantic_metrics_service.save_shared_pulls(login)
+    print("--- shared_pulls time:  %s seconds ---" % (time.time() - start_time))
     return '', 200
