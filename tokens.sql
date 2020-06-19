fa5b0f5928b1bca12054e6e44c130a3c619e23ba
Gabriel Mazzoleni Pinto <mpgabriel95@gmail.com>
2020-06-19 14:40:50 -0300

    update readme

---
+++ tokens.sql
@@ -0,0 +1,5 @@
+insert into token (token_value, last_expiration) values ('569c5d91606353118c188fa47eb6a8987cdb037c',  now() - INTERVAL '2 DAY');
+insert into token (token_value, last_expiration) values ('870ab71a61fc3ac93921d2fc4f92f901b9e4deaa',  now() - INTERVAL '2 DAY');
+insert into token (token_value, last_expiration) values ('447584eea6de24180c13c1ea9d207048605d511a',  now() - INTERVAL '2 DAY');
+insert into token (token_value, last_expiration) values ('0f72b8a2bf36730db179eb8c5e7bd101050f096a',  now() - INTERVAL '2 DAY');
+insert into token (token_value, last_expiration) values ('3bbeffe4b69d382efb0f920f1ca7c6dd282df145',  now() - INTERVAL '2 DAY');
